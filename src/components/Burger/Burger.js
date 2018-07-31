9f47f14aafa08517c3a9fcb81d3ee5cd0a31e312
Jarno Luoma-Nirva <jln@iki.fi>
2018-07-31 10:38:33 +0300

    Build burger from state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,14 +1,24 @@
 import React from 'react';
+
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 import './Burger.css';
 
 const burger = (props) => {
+    console.log('Rendering burger. Ingredients:');
+    console.log(props.ingredients);
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(ingKey => {
+            console.log(ingKey);
+            return [...Array(props.ingredients[ingKey])].map((_, i) => 
+                <BurgerIngredient key={ingKey + i} type={ingKey} />
+            )
+        });
+    console.log(transformedIngredients);
     return(
         <div className="Burger">
-            <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
-            <BurgerIngredient type="bread-bottom" />
+            <BurgerIngredient type='bread-top' />
+            {transformedIngredients}
+            <BurgerIngredient type='bread-bottom' />
         </div>
     );
 }
