3cb10e5e3e374ad4dad135933572a86b858bc02e
Jarno Luoma-Nirva <jln@iki.fi>
2018-08-01 14:44:44 +0300

    Add backdrop. Can cancel purchase.

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -2,15 +2,7 @@ import React from 'react';
 import './Backdrop.css';
 
 const backdrop = (props) => (
-    <div
-        className="Backdrop"
-        // vh refers to viewport height. -100vh is above viewport.
-        style={{
-            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: props.show ? '1' : '0'
-        }}>
-        {props.children}
-    </div>
+    props.show ? <div className="Backdrop" onClick={props.clicked}></div> : null
 );
 
 export default backdrop;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../Backdrop/Backdrop';
 import './Modal.css';
 
 const modal = (props) => (
-    <div
-        className="Modal"
-        // vh refers to viewport height. -100vh is above viewport.
-        style={{
-            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: props.show ? '1' : '0'
-        }}>
-        {props.children}
-    </div>
+    <Aux>
+        <Backdrop show={props.show} clicked={props.modalClosed} />
+        <div
+            className="Modal"
+            // vh refers to viewport height. -100vh is above viewport.
+            style={{
+                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity: props.show ? '1' : '0'
+            }}>
+            {props.children}
+        </div>
+    </Aux>
 );
 
 modal.propTypes = {
