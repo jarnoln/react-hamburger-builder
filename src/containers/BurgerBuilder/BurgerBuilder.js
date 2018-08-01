a6c431100d085fc66d851fce56a919d92d842b33
Jarno Luoma-Nirva <jln@iki.fi>
2018-08-01 11:00:31 +0300

    Can add ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
+
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
@@ -10,7 +17,24 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        console.log('Add ingredient: ' + type);
+        const oldCount = this.state.ingredients[type];
+        const newCount = oldCount + 1;
+        const newIngredients = {
+            ...this.state.ingredients
+        };
+        newIngredients[type] = newCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const newPrice = this.state.totalPrice + priceAddition;
+        this.setState({ingredients: newIngredients, totalPrice: newPrice});
+    }
+
+    removeIngredientHandler = (type) => {
     }
 
     render () {
@@ -18,7 +42,8 @@ class BurgerBuilder extends Component {
             <Aux>
                 <div> Burger Builder </div>
                 <Burger ingredients={this.state.ingredients}/>
-                <BuildControls />
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler} />
             </Aux>
         );
     }
